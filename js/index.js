0353024873d6f0e1ad7806ead16aed4e237b2196
Whitebox <sanddudu@gmail.com>
2014-03-25 21:25:32 +0800

    调整可拖动位置，禁止部分文字选中，调整窗口控制按钮

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ var pic = '';
 var comment1 = '';
 var comment2 = '';
 var color_back = '';
+var bigpicurl2x = '';
 
 //Unresizeable
 win.setResizable(0);
@@ -23,6 +24,7 @@ function getinfo() {
 		comment1 = recvjsondata.data[0].comment1;
 		comment2 = recvjsondata.data[0].comment2;
 		color_back = recvjsondata.data[0].color_back;
+		bigpicurl2x = recvjsondata.data[0]['bigpicurl@2x'];
 		document.getElementById('year').innerHTML = getyear();
 		document.getElementById('month').innerHTML = getmonth();
 		document.getElementById('weekday').innerHTML = getweekday();
@@ -31,6 +33,7 @@ function getinfo() {
 		document.getElementById('comment1').innerHTML = comment1;
 		document.getElementById('comment2').innerHTML = comment2;
 		document.getElementById('cover').style.backgroundImage = "url('" + pic + "')";
+		document.getElementById('fullimg').style.backgroundImage = "url('" + bigpicurl2x + "')";
 	},null,'utf8');
 }
 
@@ -41,6 +44,12 @@ window.onload = function () {
 	});
 	$("#page").mouseleave(function () {
 		$("#winctrl").fadeOut();
+	});
+	$("#cover").click(function () {
+		$("#fullimg").fadeIn();
+	});
+	$("#fullimg").click(function () {
+		$("#fullimg").fadeOut();
 	});
 }
 
