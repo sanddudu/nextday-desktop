340cdc96fe94f8ae64e3818a740c6a59eca0a3de
Whitebox <sanddudu@gmail.com>
2014-03-29 13:00:37 +0800

    V1.0.0

--- js/index.js
+++ js/index.js
@@ -4,52 +4,84 @@ var gui = require('nw.gui');
 var win = gui.Window.get();
 
 //Received JSON info storage
-var recvjsondata = '';
+var recvjsondata = "";
+
+//Info Date
+var pulldate = "";
 
 //Processed info storage
-var pic = '';
-var comment1 = '';
-var comment2 = '';
-var color_back = '';
-var bigpicurl2x = '';
+var pic = "";
+var comment1 = "";
+var comment2 = "";
+var color_back = "";
+var bigpicurl2x = "";
+var nowevent = "";
+var musicurl = "";
+var musicname = "";
+var musicartist = "";
 
 //Unresizeable
 win.setResizable(0);
 
 function getinfo() {
-	nodegrass.get("http://xmas2011.nxmix.com/" + getyymmdd() + "/data.json",function(data){
+	nodegrass.get("http://xmas2011.nxmix.com/" + pulldate + "/data.json",function(data){
 		recvjsondata = data;
 		recvjsondata = JSON.parse(recvjsondata);
 		pic = recvjsondata.data[0].pic;
 		comment1 = recvjsondata.data[0].comment1;
 		comment2 = recvjsondata.data[0].comment2;
 		color_back = recvjsondata.data[0].color_back;
-		bigpicurl2x = recvjsondata.data[0]['bigpicurl@2x'];
-		document.getElementById('year').innerHTML = getyear();
-		document.getElementById('month').innerHTML = getmonth();
-		document.getElementById('weekday').innerHTML = getweekday();
-		document.getElementById('day').innerHTML = getday();
-		document.getElementById('date').style.backgroundColor = color_back;
-		document.getElementById('comment1').innerHTML = comment1;
-		document.getElementById('comment2').innerHTML = comment2;
-		document.getElementById('cover').style.backgroundImage = "url('" + pic + "')";
-		document.getElementById('fullimg').style.backgroundImage = "url('" + bigpicurl2x + "')";
+		nowevent = recvjsondata.data[0]["event"];
+		bigpicurl2x = recvjsondata.data[0]["bigpicurl@2x"];
+		musicurl = recvjsondata.data[0].musicurl;
+		musicname = recvjsondata.data[0].songname;
+		musicartist = recvjsondata.data[0].artist;
+		$("#year").html(getyear());
+		$("#month").html(getmonth());
+		$("#weekday").html(getweekday());
+		$("#day").html(getday());
+		$("#date").css("background-color", color_back);
+		$("#event").html(nowevent);
+		$("#comment1").html(comment1);
+		$("#comment2").html(comment2);
+		$("#songname").html("《" + musicname + "》");
+		$("#artist").html(musicartist);
+		$("#todaymusic").attr("src", musicurl);
+		$("#cover").css("background-image", "url('" + pic + "')");
+		$("#fullimg").css("background-image", "url('" + bigpicurl2x + "')");
 	},null,'utf8');
 }
 
 window.onload = function () {
+	pulldate = getyymmdd();
 	getinfo();
 	$("#page").mouseenter(function () {
 		$("#winctrl").fadeIn();
+		$("#musicopen").fadeIn();
 	});
 	$("#page").mouseleave(function () {
 		$("#winctrl").fadeOut();
+		$("#musicopen").fadeOut();
 	});
 	$("#cover").click(function () {
 		$("#fullimg").fadeIn();
 	});
 	$("#fullimg").click(function () {
 		$("#fullimg").fadeOut();
+	});
+	$("#musicopen").click(function () {
+		$("#musicwin").fadeIn();
+		$("#musicopen").css("display", "none");
+	});
+	$("#musicexit").click(function () {
+		$("#musicwin").fadeOut();
+		$("#musicopen").css("display", "");
+	});
+	$("#min").click(function () {
+		win.minimize();
+	});
+	$("#exit").click(function () {
+		win.close();
 	});
 }
 
@@ -60,11 +92,3 @@ win.on('close', function() {
 	}
 	realexit();
 });
-
-function min () {
-	win.minimize();
-}
-
-function closewin () {
-	win.close();
-}
